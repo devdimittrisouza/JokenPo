a019ed478ee05a34875017d6e4eb21fb2d4cb944
Dimittri Souza <dimittrisouza@gmail.com>
2024-07-23 22:12:29 -0300

    Finalização da lógica de troca das imagens sorteadas, lógica de validação e exibição do resultado.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -15,7 +16,62 @@ class HomeState extends StatefulWidget {
 
 class _HomeStateState extends State<HomeState> {
 
+  var _imgPadrao = AssetImage("imagens/padrao.png");
+  var _imgPedra = "imagens/pedra.png";
+  var _imgPapel = "imagens/papel.png";
+  var _imgTesoura = "imagens/tesoura.png";
   var _textOpcao = "Escolha uma opção:";
+
+  void _opcaoSelecionada(String escolhaUsuario){
+
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("Escolha do App: " + escolhaApp);
+    print("Escolha do usuário: " + escolhaUsuario);
+
+    switch(escolhaApp){
+      case "pedra":
+        setState(() {
+          this._imgPadrao = AssetImage(_imgPedra);
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imgPadrao = AssetImage(_imgPapel);
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imgPadrao = AssetImage(_imgTesoura);
+        });
+        break;
+    }
+
+    //Usuario Vencedor
+    if(
+    (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")
+    ){
+      setState(() {
+        _textOpcao = "Você ganhou!";
+      });
+    }else if (
+    (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra")
+    ){
+      setState(() {
+        _textOpcao = "Você perdeu!";
+      });
+    }else if(escolhaUsuario == escolhaApp){
+      setState(() {
+        _textOpcao = "Empate!";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +94,16 @@ class _HomeStateState extends State<HomeState> {
             ),
           ),
 
-          //Widget para captura de gestos na tela
+          /*Widget para captura de gestos na tela
           GestureDetector(
             onTap: (){
               print("askbjdasjldas");
             },
-            child: Image.asset("imagens/padrao.png")
+            child: Image.asset(_imgPadrao)
           ),
+          */
+
+          Image(image: this._imgPadrao),
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 32),
             child: Text(_textOpcao,
@@ -58,9 +117,23 @@ class _HomeStateState extends State<HomeState> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Image.asset("imagens/pedra.png", height: 100),
-              Image.asset("imagens/papel.png", height: 100),
-              Image.asset("imagens/tesoura.png", height: 100)
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset(_imgPedra, height: 100)
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset(_imgPapel, height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset(_imgTesoura, height: 100),
+              )
+              /*
+              Image.asset(_imgPedra, height: 100),
+              Image.asset(_imgPapel, height: 100),
+              Image.asset(_imgTesoura, height: 100)
+               */
             ],
           )
         ],
